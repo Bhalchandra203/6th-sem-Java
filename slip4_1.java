dcbb787a2f3375d3d8934ea03c9ad58b358099df
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-06 13:44:41 +0530

    Update slip4_1.java

--- slip4_1.java
+++ slip4_1.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Write a Java program using Runnable interface to blink Text on the JFrame (Use 
+Swing)
  */
 package com.mycompany.practical_slip;
 
