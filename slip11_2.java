63c4fb02c25851246dff6ffc6d8955180c57ac03
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-07 00:00:21 +0530

    Update slip11_2.java

--- slip11_2.java
+++ slip11_2.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ Write a Java program to display information about all columns in the DONAR table 
+using ResultSetMetaData.
  */
 package com.mycompany.javaslip;
 
