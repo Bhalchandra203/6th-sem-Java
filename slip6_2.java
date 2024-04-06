8835018cc2a2b60047472ce8e2bd06c19dfbcf71
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-06 23:54:59 +0530

    Update slip6_2.java

--- slip6_2.java
+++ slip6_2.java
@@ -1,7 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ Write a java program using multithreading to simulate traffic signal (Use Swing).
+*/
 package com.mycompany.practical_slip;
 
 import java.util.logging.*;
