398b658e8cb635b22ea05adb41794c053e42bf39
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-07 07:51:17 +0530

    Update slip22_1.java

--- slip22_1.java
+++ slip22_1.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Write a Menu Driven program in Java for the following: Assume Employee table with 
+attributes (ENo, EName, Salary) is already created. 1. Insert 2. Update 3. Display 4. 
+Exit
  */
 package com.mycompany.javaslip;
 
