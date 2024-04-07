a027365c2032efbde9d7813d3f022f96f11e7767
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-07 07:57:43 +0530

    Update slip27_1.java

--- slip27_1.java
+++ slip27_1.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Write a Java Program to display the details of College (CID, CName, address, Year) 
+database table on JTable.
  */
 package com.mycompany.javaslip;
 
