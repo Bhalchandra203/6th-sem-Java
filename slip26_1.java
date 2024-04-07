c056a667771ca1111dc13e6d611332583af3c51a
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-07 07:55:57 +0530

    Update slip26_1.java

--- slip26_1.java
+++ slip26_1.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Write a Java program to delete the details of given employee (ENo EName Salary). 
+Accept employee ID through command line. (Use PreparedStatement Interface)
  */
 package com.mycompany.javaslip;
 
