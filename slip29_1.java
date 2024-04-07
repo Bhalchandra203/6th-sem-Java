9f2f3f4f9b7321b236ce848a4fcc15141e2be2b6
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-07 08:01:44 +0530

    Update slip29_1.java

--- slip29_1.java
+++ slip29_1.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+1. Write a Java program to display information about all columns in the DONAR table 
+using ResultSetMetaData. 
  */
 package com.mycompany.javaslip;
 
