3ebbe644dc8aed709909ae94491632cf76236437
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-15 12:56:57 +0530

    Update slip1_1.java

--- slip1_1.java
+++ slip1_1.java
@@ -1,27 +1,37 @@
 /*
- Write a Java program to display all the alphabets between ‘A’ to ‘Z’ after every 2 
-seconds.
+ Write a Java program using Multithreading to display all the alphabets between ‘A’ to 
+‘Z’ after every 2 seconds.
  */
-package com.mycompany.practical_slip;
+package com.mycompany.javaslip;
+
+
 
 import java.util.logging.*;
-import javax.swing.SwingUtilities;
 
-public class slip1_1 {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            while(true) {
+public class slip1_1
+{
+    public static void main(String[] args) 
+    {
+        Thread t = new Thread(() -> 
+        {
+            while(true) 
+            {
                 for(char ch = 'A'; ch <= 'Z'; ch++)
-                    System.out.println(ch);
+                    System.out.print(ch + " ");
+                System.out.println();
                 
-                try {
+                try 
+                {
                     Thread.sleep(2000);
-                } catch (InterruptedException ex) {
+                } 
+                catch (InterruptedException ex) 
+                {
                     Logger.getLogger(slip1_1.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 System.out.println("2 seconds are passed....");
             }
         });
         
+        t.start();
     }
 }
